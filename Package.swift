7bfff0949b95f78420951330fe62208fc0b04ad9
杨战军 <yy_13@163.com>
2026-08-29 09:36:25 +0800

    Vendor Logboard for HBuilderX

--- Package.swift
+++ Package.swift
@@ -29,11 +29,11 @@ let package = Package(
         .library(name: "MoQTHaishinKit", targets: ["MoQTHaishinKit"]),
         .library(name: "RTCHaishinKit", targets: ["RTCHaishinKit"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.5"),
-        .package(url: "https://github.com/shogo4405/Logboard.git", "2.6.0"..<"2.7.0")
-    ],
     targets: [
+        .target(
+            name: "Logboard",
+            path: "Vendor/Logboard/Sources/Logboard"
+        ),
         .binaryTarget(
             name: "libsrt",
             url: "https://github.com/HaishinKit/libsrt-xcframework/releases/download/v1.5.4/libsrt.xcframework.zip",
